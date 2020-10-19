86438eee7fe8c54db3f724ab4384c8fc18fcd29a
ShahmeerAhmad <shahmeerhafiz@gmail.com>
2020-10-19 19:55:19 +0500

    new file add

---
+++ lib/hello.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Mainclass extends StatefulWidget {
+  @override
+  _MainclassState createState() => _MainclassState();
+}
+
+class _MainclassState extends State<Mainclass> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("hello w orld"),
+    );
+  }
+}
